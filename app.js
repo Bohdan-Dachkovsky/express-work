80c9d7a01c9192d4bfb7361d1774707011d8157d
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-03-29 16:59:33 +0300

    Create post method

--- app.js
+++ app.js
@@ -9,19 +9,43 @@ const cors = require('cors')
 const uuid = require('uuid').v5
 const router = require('./routers/mainRouter.js')
 const { dirname } = require('path')
-app.use(function (req, res, next) {
-  next(createError(404))
-})
+// app.use(function (req, res, next) {
+//   next(createError(404))
+// })
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
-app.use('/', router)
-app.get('/users', async (req, res) => {
+app.use('/app', router)
+app.post('app/users/list', async (req, res) => {
+  try {
+    const { name, email, phone } = req.body
+    const userNumbers = JSON.parse(
+      await fs.readFile(path.join(__dirname, 'data/listOfContacts.js')),
+    )
+    const addUser = {
+      id: uuid(),
+      name,
+      email,
+      phone,
+    }
+    userNumbers.push(addUser)
+    await fs.writeFile('./data/listOfContacts.js', JSON.stringify(userNumbers))
+    res.status(201).json({
+      user: addUser,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  console.log(req)
+})
+app.get('app/users/list', async (req, res) => {
   try {
     const users = JSON.parse(
       await fs.readFile(path.join(__dirname, 'data/listOfContacts.js')),
     )
+
     res.status(200).json({
       users,
     })
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
-const fs = require('fs').promises
+
 // визначимо домашній роутер
 // router.get('/', (req, res) => {
 //   res.send('Це головний роутер')
@@ -12,17 +11,7 @@ router.post('/login', (req, res, next) => {
 })
 // визначимо роутер about
 router.get('/users', (req, res) => {
-  try {
-    const users = JSON.parse(
-      await fs.readFile(path.join(__dirname, 'data/listOfContacts.js')),
-    )
-    res.status(200).json({
-      users,
-    })
-  } catch (error) {
-    console.log(error)
-  }
-
+  res.json({ about: [] })
 })
 
 module.exports = router
